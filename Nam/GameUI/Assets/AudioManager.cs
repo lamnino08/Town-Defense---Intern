226411bbad9ac88bd1379f1fb01ccae4bed1c11c
Nam <nhatnamsoccer@gmail.com>
2024-07-11 15:00:06 +0700

    GameUI

---
+++ Nam/GameUI/Assets/AudioManager.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play() ;
+    }
+}
+    
